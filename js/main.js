05d72a39fda720e9ff49ac5e284b796a4fd77c4e
Marta Iachini <iachinimarta@gmail.com>
2022-07-05 16:45:53 +0200

    Impostato index.html Inizio main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let submitDom = document.getElementById('submit');
+
+submitDom.addEventListener('click',
+    function() {
+        let kmDom = document.getElementById('km');
+        let etaDom = document.getElementById('eta');
+    }
+);
